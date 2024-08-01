beb2b1861f478b6e8f315ac243e6654a0c1fba01
joaov-sha <joaovitor8832@gmail.com>
2024-08-01 19:54:05 -0300

    Criados e adicionados os arquivos referentes à atividade avaliativa

---
+++ rastreio-encomenda/src/main/java/br/edu/iftm/tspi/rastreio_encomenda/RastreioEncomendaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.tspi.rastreio_encomenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RastreioEncomendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RastreioEncomendaApplication.class, args);
+	}
+
+}
---
+++ rastreio-encomenda/src/test/java/br/edu/iftm/tspi/rastreio_encomenda/RastreioEncomendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.tspi.rastreio_encomenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RastreioEncomendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
