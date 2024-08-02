3ed4e016e553b2ec30e95ec4db8bd561c330107c
joaov-sha <joaovitor8832@gmail.com>
2024-08-01 21:41:03 -0300

    Alterado o arquivo referente aos endereços para garantir que haja um identificador

--- rastreio-encomenda/src/main/java/br/edu/iftm/tspi/rastreio_encomenda/model/Endereco.java
+++ rastreio-encomenda/src/main/java/br/edu/iftm/tspi/rastreio_encomenda/model/Endereco.java
@@ -1,10 +1,16 @@
 package br.edu.iftm.tspi.rastreio_encomenda.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Endereco {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String estado;
     private String cidade;
     private String CEP;
